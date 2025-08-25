853ddcc138d71edda01981fee5a7a992b6a2ee5f
m1kc3b <cebulski.mi@gmail.com>
2025-08-25 11:59:56 +0200

    modify Row struct by adding data field

--- src/data_model.rs
+++ src/data_model.rs
@@ -3,7 +3,20 @@ use std::collections::HashMap;
 // Represents a row in our database
 // A HasMap is great for storing pairs (colunm_name, value)
 #[derive(Debug, Clone)]
-pub struct Row(HashMap<String, String>);
+pub struct Row {
+  pub data: HashMap<String, String>
+}
+
+impl Row {
+    pub fn new(data: HashMap<String, String>) -> Self {
+        Row { data }
+    }
+
+    // Added a method to retrieve a value securely
+    pub fn get_value(&self, key: &str) -> Option<&String> {
+        self.data.get(key)
+    }
+}
 
 // Represents our database, a collection of rows
 pub struct Database {
